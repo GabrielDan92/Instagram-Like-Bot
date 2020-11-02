c61c289a3077a27a3fac9cc48b8da277722072b0
Gabriel <gaby.tzu@windowslive.com>
2020-11-02 10:31:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 import os, json, time, random, datetime, ctypes
 
 # change the working directory to target the json file
-os.chdir("C:\\Users\\\Gabriel\\Desktop\\programming\\Python")
+os.chdir("C:\\Users\\Gabriel\\Desktop\\programming\\Python")
 
 def openChrome():
     # add settings to prevent getting detected using selenium
@@ -16,18 +16,18 @@ def openChrome():
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option("useAutomationExtension", False)
     # open the browser
-    browser = webdriver.Chrome(executable_path=r"C:\Users\Gabriel\Desktop\programming\chromedriver.exe", options=options)
+    browser = webdriver.Chrome(executable_path=r"C:\Users\Gabriel\Desktop\programming\Python\chromedriver2.exe", options=options)
     browser.delete_all_cookies()
     browser.implicitly_wait(10)
-    browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-    "source": """
-        Object.defineProperty(navigator, 'webdriver', {
-        get: () => undefined
-        })
-    """
-    })
-    browser.execute_cdp_cmd("Network.enable", {})
-    browser.execute_cdp_cmd("Network.setExtraHTTPHeaders", {"headers": {"User-Agent": "browser1"}})
+    # browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+    # "source": """
+    #     Object.defineProperty(navigator, 'webdriver', {
+    #     get: () => undefined
+    #     })
+    # """
+    # })
+    # browser.execute_cdp_cmd("Network.enable", {})
+    # browser.execute_cdp_cmd("Network.setExtraHTTPHeaders", {"headers": {"User-Agent": "browser1"}})
 
     # open the json file containing the username and password
     with open('config.json','r') as f:
@@ -35,8 +35,9 @@ def openChrome():
 
     # log in
     browser.get("https://www.instagram.com")
-    username = browser.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input")
-    password = browser.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input")
+    username = browser.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input")
+    password = browser.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input")
+
     time.sleep(random.uniform(1.0, 2.0))
     username.send_keys(config['user']['name'])
     time.sleep(random.uniform(1.0, 2.0))
@@ -44,6 +45,7 @@ def openChrome():
     time.sleep(random.uniform(1.0, 2.0))
     password.submit()
     time.sleep(5)
+    browser.find_element_by_xpath("/html/body/div[2]/div/div/div/div[2]/button[1]").click()
 
     return browser
 #==============================================================================================
@@ -87,6 +89,11 @@ for _ in range(len(hshtgList) * 4):
     log(" Navigating to #" + hshtgList[j] + "...")
     browser.get("https://www.instagram.com/explore/tags/" + hshtgList[j] + "/")
 
+    try:
+        browser.find_element_by_xpath("/html/body/div[2]/div/div/div/div[2]/button[1]").click()
+    except:
+        pass
+    
     # click on the first image
     browser.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div/div[1]/div[1]/a/div").click()
     likeCounter = 0
@@ -102,7 +109,7 @@ for _ in range(len(hshtgList) * 4):
             # heart = browser.find_element_by_class_name("wpO6b").find_element_by_css_selector("svg:first-child") 
             heart = browser.find_element_by_xpath("/html/body/div[4]/div[2]/div/article/div[3]/section[1]/span[1]/button")
             value = heart.get_attribute("outerHTML").split("=")
-            value = value[4].split("class")
+            value = value[5].split("class")
             value = value[0].replace('"', "").strip()
 
             # like the picture if it's not already liked, based on the attribute retrieved above
@@ -117,7 +124,8 @@ for _ in range(len(hshtgList) * 4):
             log("Next image.")
             browser.find_element_by_class_name("_65Bje").click()
 
-        except:
+        except Exception as e:
+            print(str(e))
             log(str(likeCounter) + " images liked. Going to the next hashtag due to error encountered.")
             errorCounter += 1
             break
