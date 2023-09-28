5c10849faba45eb31cfd9e68c3c07ebb9608012c
Gabriel <gaby.tzu@windowslive.com>
2023-09-28 18:37:47 +0300

    Refactor to OOP

--- main.py
+++ main.py
@@ -1,137 +1,198 @@
 from selenium import webdriver
-import os, json, time, random, datetime, ctypes
+import os
+import json
+import time
+import random
+import datetime
+import ctypes
 
-# change the working directory to target the json file
-os.chdir("C:\\Users\\Gabriel\\Desktop\\programming\\Python")
 
-def openChrome():
-    # add settings to prevent getting detected using selenium
-    options = webdriver.ChromeOptions()
-    options.add_argument("--disable-extensions")
-    options.add_argument("--disable-infobars")
-    options.add_argument("--disable-plugins-discovery")
-    options.add_argument("--disable-blink-features=AutomationControlled")
-    options.add_argument("--incognito")
-    #options.add_argument("--headless")
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    options.add_experimental_option("useAutomationExtension", False)
-    # open the browser
-    browser = webdriver.Chrome(executable_path=r"C:\Users\Gabriel\Desktop\programming\Python\chromedriver2.exe", options=options)
-    browser.delete_all_cookies()
-    browser.implicitly_wait(10)
-    # browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
-    # "source": """
-    #     Object.defineProperty(navigator, 'webdriver', {
-    #     get: () => undefined
-    #     })
-    # """
-    # })
-    # browser.execute_cdp_cmd("Network.enable", {})
-    # browser.execute_cdp_cmd("Network.setExtraHTTPHeaders", {"headers": {"User-Agent": "browser1"}})
+class ChromeOptionsBuilder:
+    """
+    A class for building Chrome webdriver options.
+    """
+    @staticmethod
+    def build_options():
+        """
+        Build Chrome webdriver options with specific configurations to prevent getting detected while using selenium.
 
-    # open the json file containing the username and password
-    with open('config.json','r') as f:
-        config = json.load(f)
+        Returns:
+            selenium.webdriver.chrome.options.Options: Chrome options object.
+        """
+        options = webdriver.ChromeOptions()
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-infobars")
+        options.add_argument("--disable-plugins-discovery")
+        options.add_argument("--disable-blink-features=AutomationControlled")
+        options.add_argument("--incognito")
+        options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        options.add_experimental_option("useAutomationExtension", False)
 
-    # log in
-    browser.get("https://www.instagram.com")
-    username = browser.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[1]/div/label/input")
-    password = browser.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div/div[2]/div/label/input")
+        return options
 
-    time.sleep(random.uniform(1.0, 2.0))
-    username.send_keys(config['user']['name'])
-    time.sleep(random.uniform(1.0, 2.0))
-    password.send_keys(config['user']['password'])
-    time.sleep(random.uniform(1.0, 2.0))
-    password.submit()
-    time.sleep(5)
-    browser.find_element_by_xpath("/html/body/div[2]/div/div/div/div[2]/button[1]").click()
 
-    return browser
-#==============================================================================================
+class InstagramLogin:
+    """
+    A class for handling Instagram login functionality.
+    """
+    @staticmethod
+    def login(browser, username, password):
+        """
+        Log in to Instagram using the provided credentials.
 
-j = -1
-errorCounter = 0
-totalLikes = 0
-likeCounter = 0
-likeLimit = 50
-sleepTime = 15 #minutes
+        Args:
+            browser (selenium.webdriver.chrome.webdriver.WebDriver): The WebDriver instance.
+            username (str): Instagram username.
+            password (str): Instagram password.
+        """
+        browser.get("https://www.instagram.com")
+        username_field = browser.find_element_by_xpath("//input[@name='username']")
+        password_field = browser.find_element_by_xpath("//input[@name='password']")
 
-def log(string):
-    print(str(datetime.datetime.now().hour).zfill(2) + ":" + str(datetime.datetime.now().minute).zfill(2) + " " + string)
+        time.sleep(random.uniform(1.0, 2.0))
+        username_field.send_keys(username)
+        time.sleep(random.uniform(1.0, 2.0))
+        password_field.send_keys(password)
+        time.sleep(random.uniform(1.0, 2.0))
+        password_field.submit()
+        time.sleep(5)
+        browser.find_element_by_xpath("//button[text()='Log In']").click()
 
-hshtgList = ["fujifilm_xseries", "ig_romania", "bealpha", "fujifilm", "romania", "fujifilmxt30", "sonya7riii"]
 
-for _ in range(len(hshtgList) * 4):
-    totalLikes += likeCounter
-    j += 1
-    print("Total liked images so far: " + str(totalLikes))
+class InstagramBot:
+    """
+    A class representing an Instagram automation bot.
+    """
+    def __init__(self):
+        """
+        Initialize the InstagramBot instance.
+        """
+        self.browser = None
+        self.j = -1
+        self.error_counter = 0
+        self.total_likes = 0
+        self.like_counter = 0
+        self.like_limit = 50
+        self.sleep_time = 15  # minutes
+        self.hashtags = ["fujifilm_xseries", "ig_romania", "bealpha", "fujifilm", "romania", "fujifilmxt30", "sonya7riii"]
 
-    if errorCounter == 6:
-        log("Five errors encountered. Terminating session...")
-        break
-    elif totalLikes == 1500:
-        log("1500 likes done. Terminating session...")
-        break
+        self.chrome_driver_path = os.environ.get('CHROME_DRIVER_PATH')
+        if not self.chrome_driver_path:
+            raise ValueError("CHROME_DRIVER_PATH environment variable not set.")
 
-    if j != 0:
-        log("Sleeping for " + str(sleepTime) + " minutes and closing the browser.")
-        browser.quit()
-        time.sleep((sleepTime/2) *60)
-        log(str(sleepTime/2) + " minutes left...")
-        time.sleep((sleepTime/2) *60)
+    def log(self, string):
+        """
+        Log a message with a timestamp.
 
-    if j == len(hshtgList):
-        j = 0
+        Args:
+            string (str): The message to log.
+        """
+        print(str(datetime.datetime.now().hour).zfill(2) + ":" + str(datetime.datetime.now().minute).zfill(2) + " " + string)
 
-    browser = openChrome()
-    browser.get("https://www.instagram.com/explore/tags/" + hshtgList[j] + "/")
-    log(" Navigating to #" + hshtgList[j] + "...")
-    browser.get("https://www.instagram.com/explore/tags/" + hshtgList[j] + "/")
+    def open_browser(self):
+        """
+        Open a Chrome browser with configured options.
+        """
+        options = ChromeOptionsBuilder.build_options()
+        self.browser = webdriver.Chrome(executable_path=self.chrome_driver_path, options=options)
+        self.browser.delete_all_cookies()
+        self.browser.implicitly_wait(10)
 
-    try:
-        browser.find_element_by_xpath("/html/body/div[2]/div/div/div/div[2]/button[1]").click()
-    except:
-        pass
-    
-    # click on the first image
-    browser.find_element_by_xpath("/html/body/div[1]/section/main/article/div[2]/div/div[1]/div[1]/a/div").click()
-    likeCounter = 0
+    def close_browser(self):
+        """
+        Close the Chrome browser if it's open.
+        """
+        if self.browser:
+            self.browser.quit()
 
-    while True:
-        if likeCounter == likeLimit:
-            log(str(likeLimit) + " liked images. Moving to the next hashtag.")
-            break
+    def navigate_to_hashtag(self, hashtag):
+        """
+        Navigate to a specific hashtag on Instagram.
+
+        Args:
+            hashtag (str): The hashtag to navigate to.
+        """
+        self.browser.get("https://www.instagram.com/explore/tags/" + hashtag + "/")
+        self.log(" Navigating to #" + hashtag + "...")
+
+        try:
+            self.browser.find_element_by_xpath("/html/body/div[2]/div/div/div/div[2]/button[1]").click()
+        except:
+            pass
+
+    def like_image(self):
+        """
+        Like an image on Instagram.
+        """
         try:
             time.sleep(random.uniform(2.0, 3.0))
-
-            # retrieve the heart attribute (Liked/Do not like)
-            # heart = browser.find_element_by_class_name("wpO6b").find_element_by_css_selector("svg:first-child") 
-            heart = browser.find_element_by_xpath("/html/body/div[4]/div[2]/div/article/div[3]/section[1]/span[1]/button")
+            heart = self.browser.find_element_by_xpath("//button[@aria-label='Like']").click()
             value = heart.get_attribute("outerHTML").split("=")
             value = value[5].split("class")
             value = value[0].replace('"', "").strip()
 
-            # like the picture if it's not already liked, based on the attribute retrieved above
             if value == "Like":
                 heart.click()
-                likeCounter += 1
-                log("Liking image. " + str(likeCounter) + " liked images so far.")
+                self.like_counter += 1
+                self.log("Liking image. " + str(self.like_counter) + " liked images so far.")
                 time.sleep(random.uniform(2.5, 4.0))
             else:
-                log("image already liked. Skipping it. " + str(likeCounter) + " liked images so far.")
+                self.log("image already liked. Skipping it. " + str(self.like_counter) + " liked images so far.")
 
-            log("Next image.")
-            browser.find_element_by_class_name("_65Bje").click()
+            self.log("Next image.")
+            self.browser.find_element_by_class_name("_65Bje").click()
 
         except Exception as e:
             print(str(e))
-            log(str(likeCounter) + " images liked. Going to the next hashtag due to error encountered.")
-            errorCounter += 1
-            break
+            self.log(str(self.like_counter) + " images liked. Going to the next hashtag due to an error encountered.")
+            self.error_counter += 1
 
-totalLikes += likeCounter
-log("Total images liked: " + str(totalLikes))
-log("Quiting browser...")
-browser.quit()
-ctypes.windll.user32.MessageBoxW(0, "The script has been completed!", "Done", 0)
+    def run(self):
+        """
+        Run the Instagram automation bot.
+        """
+        try:
+            self.open_browser()
+            with open('config.json', 'r') as f:
+                config = json.load(f)
+            InstagramLogin.login(self.browser, config['user']['name'], config['user']['password'])
+
+            for _ in range(len(self.hashtags) * 4):
+                self.total_likes += self.like_counter
+                self.j += 1
+                print("Total liked images so far: " + str(self.total_likes))
+
+                if self.error_counter == 6:
+                    self.log("Five errors encountered. Terminating session...")
+                    break
+                elif self.total_likes == 1500:
+                    self.log("1500 likes done. Terminating session...")
+                    break
+
+                if self.j != 0:
+                    self.log("Sleeping for " + str(self.sleep_time) + " minutes and closing the browser.")
+                    self.close_browser()
+                    time.sleep((self.sleep_time / 2) * 60)
+                    self.log(str(self.sleep_time / 2) + " minutes left...")
+                    time.sleep((self.sleep_time / 2) * 60)
+
+                if self.j == len(self.hashtags):
+                    self.j = 0
+
+                self.navigate_to_hashtag(self.hashtags[self.j])
+                self.like_counter = 0
+
+                while self.like_counter < self.like_limit:
+                    self.like_image()
+
+            self.total_likes += self.like_counter
+            self.log("Total images liked: " + str(self.total_likes))
+
+        finally:
+            self.close_browser()
+            ctypes.windll.user32.MessageBoxW(0, "The script has been completed!", "Done", 0)
+
+
+if __name__ == "__main__":
+    bot = InstagramBot()
+    bot.run()
